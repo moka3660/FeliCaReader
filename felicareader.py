d82f16a86fc9114fe976482f63b108ddd53d76ec
kenta <moka12almukha30@gmail.com>
2017-06-28 23:25:04 +0900

    update
    1)GPIOでLEDをつける用
    2)  動いていない！！！！！！！

--- felicareader.py
+++ felicareader.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-This script is for register of NFC cards.
-Run this script, touch your card on censor,
-and then its IDm will be written into file.
+###
 '''
 
 import binascii
@@ -13,12 +11,39 @@ import time
 import nfc
 from RPi import GPIO
 
+class FeliCaManager(object):
+    _LED_RED = 17
+    _LED_GREEN = 27
+    _LED_BLUE = 22
+
+    @property
+    def __enter__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self._LED_RED,GPIO.OUT)
+        GPIO.setup(self._LED_GREEN,GPIO.OUT)
+        GPIO.setup(self._LED_BLUE,GPIO.OUT)
+        GPIO.output(self._LED_RED,0)
+        GPIO.output(self._LED_GREEN,0)
+        GPIO.output(self._LED_BLUE,0)
+        print "Started FeliCaReader !!"
+        return self
+
+    def __exit__(self):
+        GPIO.cleanup()
+        print "Stopped FeliCaReader."
+
+    def read(self):
+        GPIO.output(self._LED_BLUE, 1)
+        time.sleep(0.5)
+        GPIO.output(self._LED_BLUE, 0)
+        print "Successfully get IDm."
+
 def main(ids_csv_filename):
-    with open(ids_csv_filename, 'a') as ids_csv_file:
+    with open(ids_csv_filename, 'a') as ids_csv_file, FeliCaManager() as felica_manager:
         clf = nfc.ContactlessFrontend('usb')
         print "Press ^C to quit ..."
         while True:
-            time.sleep(1)
+            time.sleep(1.0)
 
             tag = clf.connect(rdwr={'on-connect': None})
             if not isinstance(tag, nfc.tag.tt3.Type3Tag):
@@ -26,6 +51,7 @@ def main(ids_csv_filename):
                 continue
 
             idm = binascii.hexlify(tag.idm)
+            felica_manager.read()
             print "ID: " + idm
             now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
             ids_csv_file.write("{},{}\n".format(now, idm))
