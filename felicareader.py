f9452043ed2e8ec00ff21967c4927dadbd4655a9
kenta <moka12almukha30@gmail.com>
2017-07-23 17:25:25 +0900

    update felicareader.py
    1)socketで送信する部分を追記
    2)RasPiの番号はまだ

--- felicareader.py
+++ felicareader.py
@@ -7,16 +7,43 @@ import binascii
 import datetime
 import sys
 import time
+import socket
 
 import nfc
 from RPi import GPIO
 
+HOST = '---'
+PORT = ---
+INTERVAL = 3
+RETRYTIMES = 5
+
 finIDm = 77408918205372968  #0113030040141A28
+
+def socket_connect(host, port, interval, retries):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    for x in range(retries):
+        try:
+            sock.connect((host, port))
+            return sock
+        except socket.error:
+            print "Wait"+str(interval)+"sec"
+            time.sleep(interval)
+
+    sock.close()
+    return None
 
 def main(ids_csv_filename):
     with open(ids_csv_filename, 'a') as ids_csv_file:
         clf = nfc.ContactlessFrontend('usb')
-#        GPIO.cleanup()
+
+        sock = socket_connect(HOST, PORT, INTERVAL, RETRYTIMES)
+
+        if sock is None:
+            print "system exit:connection error"
+            sys.exit(0)
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(17,GPIO.OUT) #Red
         GPIO.setup(27,GPIO.OUT) #Green
@@ -46,11 +73,14 @@ def main(ids_csv_filename):
             print "Successfully get IDm."
             print "ID: " + idm
 
+            sock.send(idm)
+
             now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
             ids_csv_file.write("{},{}\n".format(now, idm))
 
             idm_dec = int(idm,16)
             if idm_dec ==  finIDm:
+                sock.close()
                 break
 
         GPIO.cleanup()
