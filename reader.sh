d0205a1943cf09f359818be62d2efbd8e768f353
kenta <moka12almukha30@gmail.com>
2017-09-19 21:22:59 +0900

    update fix reader.sh
    1)reader.shをidm_reader用に改造
    2)一定時間ごとに再起動し、その際に実行されることを想定

--- reader.sh
+++ reader.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #echo 0
-dir=/home/pi/nfcpy/experiments3
+dir=/home/pi/nfcpy/septem1
 file_name=$(date '+%Y%m%d%H%M').csv
 #echo 1
-#jobs -l | awk '{print "sudo kill " $2;}'|sh
-#ps -afx | grep felicareader | grep -v grep | sed 2d | awk '{print "asudo kill" $1;}'|sh
+sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa' $dir morimoto@153.126.194.52:/home/morimoto/
 #echo 2
-sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa' $dir morimoto@153.126.194.52:/home/morimoto/exp
+sudo python /home/pi/nfcpy/idm_reader.py $dir/$file_name &
 #echo 3
-sudo python /home/pi/nfcpy/felicareader8.py $dir/$file_name &
-#echo 4
