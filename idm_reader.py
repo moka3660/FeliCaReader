57c5c9d3420e457c9d0fa405473bbccdd69ba8fc
kenta <moka12almukha30@gmail.com>
2017-09-19 10:21:44 +0900

    update idm_reader
    1)csvに保存(idmのみ)
    2)一部print変更

--- idm_reader.py
+++ idm_reader.py
@@ -2,6 +2,7 @@
 
 import binascii
 import nfc
+import sys
 
 finIDm = 72340248454488070  #010101129C17E006 [ICOCA]
 
@@ -10,6 +11,7 @@ class MyCardReader(object):
     def on_connect(self, tag):
         print "touched"
         self.idm = binascii.hexlify(tag.idm)
+        # LED
         return True
 
     def read_id(self):
@@ -19,17 +21,26 @@ class MyCardReader(object):
         finally:
             clf.close()
 
-def main():
+def main(ids_csv_filename):
     cr = MyCardReader()
     while True:
         print "touch card:"
         cr.read_id()
-        print "released"
-        print cr.idm
+#        print "released"
+#        print cr.idm
 
-        idm_dec =int(cr.idm,16)
-        if idm_dec == finIDm:
-            break
+        with open(ids_csv_filename, 'a') as ids_csv_file:   #csv open
+            print "Successfully get IDm."
+            print "IDm: " + cr.idm
+
+            ids_csv_file.write("{}\n".format(cr.idm))
+            ids_csv_file.close()
+
+            idm_dec =int(cr.idm,16)
+            if idm_dec == finIDm:
+                break
+
+    print "Stopped idm_reader"
 
 
 if __name__ == '__main__':
