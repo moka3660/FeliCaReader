f864ef9f733b872f313ae216af512bdc9aeb00e4
kenta <moka12almukha30@gmail.com>
2017-09-20 17:16:40 +0900

    fix reader.sh(change dir)
    1)ディレクトリにFeliCaReaderをはさんだ

--- reader.sh
+++ reader.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #echo 0
-dir=/home/pi/nfcpy/septem1
+dir=/home/pi/nfcpy/FeliCaReader/septem1
 file_name=$(date '+%Y%m%d%H%M').csv
 #echo 1
 sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa' $dir morimoto@153.126.194.52:/home/morimoto/
 #echo 2
-sudo python /home/pi/nfcpy/idm_reader.py $dir/$file_name &
+sudo python /home/pi/nfcpy/FeliCaReader/idm_reader.py $dir/$file_name &
 #echo 3
