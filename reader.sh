ca0f6e205495f13c2d99a2b5160072d0488ad7d2
kenta <moka12almukha30@gmail.com>
2017-09-23 11:17:59 +0900

    update reader.sh
    1)dirにras0追加
    2)id_rsaに0を追記

--- reader.sh
+++ reader.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #echo 0
-dir=/home/pi/nfcpy/FeliCaReader/septem1
+sakura=XXXXX
+dir=/home/pi/nfcpy/FeliCaReader/septem1/ras0
 file_name=$(date '+%Y%m%d%H%M').csv
 #echo 1
-sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa' $dir morimoto@153.126.194.52:/home/morimoto/
+sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa0' $dir sakura:/home/morimoto/
 #echo 2
 sudo python /home/pi/nfcpy/FeliCaReader/idm_reader.py $dir/$file_name &
 #echo 3
