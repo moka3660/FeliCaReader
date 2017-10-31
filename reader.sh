0e7c2ec085bc3032113ea60639a1aae73f12ffb9
kenta <moka12almukha30@gmail.com>
2017-10-31 13:13:13 +0900

    fix reader.sh
    file_nameの"分"を削除(年月日時)に

--- reader.sh
+++ reader.sh
@@ -2,7 +2,7 @@
 #echo 0
 sakura=XXXXX
 dir=/home/pi/nfcpy/FeliCaReader/septem1/ras0
-file_name=$(date '+%Y%m%d%H%M').csv
+file_name=$(date '+%Y%m%d%H').csv
 #echo 1
 sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa0' $dir sakura:/home/morimoto/septem1/
 #echo 2
