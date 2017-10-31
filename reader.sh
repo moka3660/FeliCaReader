dd1876374176b6a1269c9faf3b74967c92675e0b
kenta <moka12almukha30@gmail.com>
2017-10-31 13:28:49 +0900

    fix reader.sh
    sakura => $sakura

--- reader.sh
+++ reader.sh
@@ -4,7 +4,7 @@ sakura=XXXXX
 dir=/home/pi/nfcpy/FeliCaReader/septem1/ras0
 file_name=$(date '+%Y%m%d%H').csv
 #echo 1
-sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa0' $dir sakura:/home/morimoto/septem1/
+sudo rsync -aruz -e 'ssh -p 443 -i /home/pi/.ssh/id_rsa0' $dir $sakura:/home/morimoto/septem1/
 #echo 2
 sudo python /home/pi/nfcpy/FeliCaReader/idm_reader.py $dir/$file_name &
 #echo 3
